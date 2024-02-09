cab51501315325cf715d0f833953236f2794bfcc
MaimulaK <maimulakonstantin@gmail.com>
2024-02-09 16:40:48 +0300

    release full

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs').promises;
-const joke = require('./topics/pass');
+const tableTennis = require('./topics/pass');
 
 async function read(file) {
   const text = JSON.parse(await fs.readFile(file, 'utf-8'));
@@ -10,22 +10,24 @@ async function read(file) {
 async function start(start, topic) {
   const res = await inquirer.prompt(start);
 
-  const quiz = await inquirer.prompt(topic);
+  const quz = await inquirer.prompt(topic);
 
-  if (topic === joke) {
-    console.log('На карте недостаточно средств! 😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀');
-  } else {
-    const score = Object.values(quiz).reduce(((acc, el) => acc + +el), 0);
+  if (topic === tableTennis) {
+    const score = Object.values(quz).reduce((acc, el) => acc + +el, 0);
     console.clear();
-    console.log(`По итогам игры ${res.username} набрал ${score} баллов из 10`);
+    console.log(`Игрок ${res.username} набрал ${score} очков`);
+  } else {
+    const score = Object.values(quz).reduce((acc, el) => acc + +el, 0);
+    console.clear();
+    console.log(`Игрок ${res.username} набрал ${score} очков`);
   }
 }
 
 async function quiz() {
   const prg = process.argv[2];
   const init = await read('./topics/start.json');
-  const commonQuestions = (await read('./topics/questions.json'));
-  const theBestQuestions = joke;
+  const commonQuestions = await read('./topics/questions.json');
+  const theBestQuestions = tableTennis;
   if (prg == 0) {
     await start(init, commonQuestions);
   } else {
